a703a524b6453fc43f3d87e24525ba17339d5fed
takvir-imran <takvir.imran.cse@gmail.com>
2023-01-30 16:39:03 +0600

    bug solved 1

--- app/src/main/java/com/example/usd_to_bdt_converstion_with_tax/Conversion.kt
+++ app/src/main/java/com/example/usd_to_bdt_converstion_with_tax/Conversion.kt
@@ -1,3 +1,3 @@
 package com.example.usd_to_bdt_converstion_with_tax
 
-data class Conversion(var usd: Double, var conversionRate: Double = 106.0, var tax: Double= 32.0)
+data class Conversion(var usd: Double, var tax: Double= 32.0, var conversionRate: Double = 106.0)
--- app/src/main/java/com/example/usd_to_bdt_converstion_with_tax/MainActivity.kt
+++ app/src/main/java/com/example/usd_to_bdt_converstion_with_tax/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.usd_to_bdt_converstion_with_tax
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.core.view.isEmpty
 import androidx.databinding.DataBindingUtil
 import com.example.usd_to_bdt_converstion_with_tax.databinding.ActivityMainBinding
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    lateinit var convertRate : Conversion
+    lateinit var conversion : Conversion
+    val df = DecimalFormat("#.##")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,11 +22,30 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnConvert.setOnClickListener {
 
-            var conversion= Conversion(binding.usdAmount.text.toString().toDouble())
-            var taxAmount = conversion.usd +(conversion.usd * (conversion.tax/100))
 
+            if(binding.tax.text!!.isEmpty() && binding.conversionRate.text!!.isEmpty()){
+                conversion= Conversion(binding.usdAmount.text.toString().toDouble())
+            }
+            else if(binding.conversionRate.text!!.isEmpty()){
+                conversion= Conversion(binding.usdAmount.text.toString().toDouble(), binding.tax.text.toString().toDouble())
+            }
+            else if(binding.tax.text!!.isEmpty()){
+                conversion= Conversion(binding.usdAmount.text.toString().toDouble(), conversionRate= binding.conversionRate.text.toString().toDouble())
+            }
+            else{
+                conversion= Conversion(binding.usdAmount.text.toString().toDouble(), conversionRate= binding.conversionRate.text.toString().toDouble(), tax = binding.tax.text.toString().toDouble() )
+            }
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(binding.btnConvert.windowToken, 0)
 
-            Toast.makeText(this, "${binding.tax.text}", Toast.LENGTH_LONG).show()
+            var taxAmount = (conversion.usd * (conversion.tax/100))
+            var convertedUSD =conversion.usd *  conversion.conversionRate
+            var convertedTAX = taxAmount * conversion.conversionRate
+
+            binding.linearLayoutBreakdown.visibility= View.VISIBLE
+            binding.usdConversion.text = "Actual Bill: ${conversion.usd} USD x ${conversion.conversionRate} Taka = $convertedUSD Taka"
+            binding.taxConversion.text = "Tax: $taxAmount USD x ${conversion.conversionRate} Taka = ${df.format(convertedTAX)} Taka"
+            binding.totalAmount.text = "Total: ${conversion.usd+taxAmount} USD x ${conversion.conversionRate} Taka = ${df.format(convertedUSD+convertedTAX)} Taka"
         }
     }
 }
